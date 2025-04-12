643467055ad7f887f149fbfc45998cef76d27380
Jason Chen <jchen33@umbc.edu>
2025-04-12 17:32:47 -0400

    added appbar for live environment

--- lib/screens/live_view.dart
+++ lib/screens/live_view.dart
@@ -1,12 +1,22 @@
-
 import 'package:flutter/material.dart';
 
 class LiveView extends StatefulWidget {
   const LiveView({super.key});
 
-  State<LiveView> createState(){
-    return 
+  State<LiveView> createState() {
+    return _LiveView();
   }
 }
 
-class _LiveView extends State<>
+class _LiveView extends State<LiveView> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF47E0F8),
+        title: Text('Live Environment', 
+        style: TextStyle(color: Colors.black)),
+        
+      ),
+    );
+  }
+}
